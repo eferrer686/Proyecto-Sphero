e92babe250f293be6b3eaf4b8380d0b735780baa
eferrer686 <edfmgr@hotmail.com>
2018-10-27 03:40:48 -0500

    Revert "Wall Colission"
    This reverts commit bab08cba7265b9bcaf665e915567d695a3ea96ba.

--- pygamePrueba.py
+++ pygamePrueba.py
@@ -1,6 +1,8 @@
 import pygame, sys
 from pygame.locals import *
-
+HEIGHT_WINDOW = 400
+WIDTH_WINDOW = 500
+###Clase Sphero
 class Sphero(object):
     def __init__(self, posX, posY, vel, MyImage):
         self.posX = posX
@@ -18,60 +20,27 @@ class Sphero(object):
     def get_vel(self):
         return self.vel
 
-    def MLeft(self, posX, vel):
-            self.posX -= vel
+    def MLeft(self, vel):
+        self.posX -= (vel)
 
+    def MRight(self, vel):
+        self.posX += (vel)
+    def MoveX(self,vel):
+        self.posX += vel
+    def MoveY(self,vel):
+        self.posY +=vel
+    def MUp(self, vel):
+        self.posY -= (vel)
 
-    def MRight(self, posX, vel):
-            self.posX += int(vel)
-
-    def MUp(self, posY, vel):
-            self.posY -= int(vel)
-
-    def MDown(self, posY, vel):
-            self.posY += int(vel)
-
-class Pared(object):
-    def __init__(self, posX, posY, Height, Width):
-        self.Width = Width
-        self.Height = Height
-        self.posX = posX
-        self.posY = posY
-        self.Xi = self.posX - (self.Width/2)
-        self.Xf = self.posX + (self.Width/2)
-        self.Yi = self.posY - (self.Height/2)
-        self.Yf = self.posY + (self.Height/2)
-        self.surf = pygame.Surface((self.Width, self.Height))
-        self.surf.fill((0, 0, 0))
-        self.rect = self.surf.get_rect()
-
-
-    def get_posX(self):
-        return self.posX
-
-    def get_posY(self):
-        return self.posY
-
-    def get_Height(self):
-        return self.Height
-
-    def get_Width(self):
-        return self.Width
-
-    def Collide(self, spheroPosX, spheroPosY):
-        if(spheroPosX + 25 > self.Xi and spheroPosX - 25 < self.Xf and spheroPosY + 25 > self.Yi and spheroPosY - 25 < self.Yf):
-            return True
-        else:
-            return False
-
+    def MDown(self, vel):
+        self.posY += (vel)
 
 color = pygame.Color(31, 64, 195)
 
-#####################################
-
+#Incia Juego
 pygame.init()
 #Ventana
-venta = pygame.display.set_mode((500, 400))
+venta = pygame.display.set_mode((WIDTH_WINDOW, HEIGHT_WINDOW))
 
 #Rectangulo
 Rectangulo = pygame.Rect(20, 20, 100, 50)
@@ -79,40 +48,31 @@ Rectangulo = pygame.Rect(20, 20, 100, 50)
 #Color blanco
 white = (255, 255, 255)
 myImage = pygame.image.load("img/Sphero.png")
-pygame.draw.circle(myImage,(0,0,0),(25,25),25,0)
 
 pygame.display.set_caption("Laberinto")
 
 #Crear sphero
 sphero = Sphero(50, 50, 2, myImage)
-#Crear Pared
-pared = Pared(100, 100, 100, 40)
-wall = pygame.Rect(pared.get_posX(), pared.get_posY(), pared.get_Width(), pared.get_Height())
-
+moveRight = True
+moveDown = True
 while True:
     venta.fill(white)
     venta.blit(sphero.MyImage, (sphero.posX, sphero.posY))
-    #Dibujar Pared
-    venta.blit(pared.surf, (pared.posX, pared.posY))
-    
-    #pygame.draw.Rect(venta, color, wall, 0)
     #pygame.draw.rect(venta, color, Rectangulo, 0)
+    ##Eje X
+    if(sphero.posX<=50 and moveRight != True):
+        moveRight = True
+    elif(sphero.posX>=WIDTH_WINDOW-50 and moveRight == True):
+        moveRight = False
+    direccionX = 1 if(moveRight) else -1
+    sphero.MoveX(direccionX)
+    ##Eje Y
+    if(sphero.posY<=50 and moveDown != True):
+        moveDown = True
+    elif(sphero.posY>=HEIGHT_WINDOW-50 and moveDown == True):
+        moveDown = False
+    direccionY = 1 if(moveDown) else -1
+    sphero.MoveY(direccionY)
 
-
-    for event in pygame.event.get():
-        #Cerrar Ventana
-        if(event.type == QUIT):
-            pygame.quit()
-            sys.exit()
-        #Movimiento
-        elif(event.type == KEYDOWN):
-            if(event.key == K_LEFT):
-                sphero.MLeft(sphero.get_posX(), 1)
-            if(event.key == K_UP):
-                sphero.MUp(sphero.get_posY(), 1)
-            if(event.key == K_RIGHT):
-                sphero.MRight(sphero.get_posX(), 1)
-            elif(event.key == K_DOWN):
-                sphero.MDown(sphero.get_posX(), 1)
 
     pygame.display.update()
